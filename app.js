1d72620ec20464606dac1da371c246ae7181a97c
leeyeonji28 <yeon102800@gmail.com>
2022-05-26 19:38:31 +0900

    수정 API

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mysql from "mysql2/promise";
+import cors from "cors";
 
 // db 연결
 const pool = mysql.createPool({
@@ -14,6 +15,7 @@ const pool = mysql.createPool({
 });
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 const port = 3000;
 
@@ -38,7 +40,6 @@ app.get("/wise-sayings/random", async (req, res) => {
   }
 
   // 조회수 증가
-
   wiseSayingRow.hitCount++; // 방법 1. hitCount을 증가시키고 query에 ?를 넣어줌
 
   await pool.query(
@@ -56,6 +57,65 @@ app.get("/wise-sayings/random", async (req, res) => {
     resultCode: "S-1",
     msg: "성공",
     data: wiseSayingRow,
+  });
+});
+
+// 수정
+app.patch("/wise-sayings/:id", async (req, res) => {
+  const { id } = req.params;
+  const [[wiseSayingRow]] = await pool.query(
+    `
+    SELECT *
+    FROM wise_saying
+    WHERE id = ?
+    `,
+    [id]
+  );
+
+  if (wiseSayingRow === undefined) {
+    res.status(404).json({
+      resultCode: "F-1",
+      msg: "404 not found",
+    });
+    return;
+  }
+
+  // 수정 할 수 있는 데이터
+  const {
+    // content, author, goodLikeCount, badLikeCount -> 하나만 수정할라고 해도 데이터를 다 적어야 함, 만약 안적으면 값은 undefined 값이 나옴
+    content = wiseSayingRow.content, // req.body 안에 content가 없으면 wiseSayingRow.content이 들어간다. 즉, 값을 적지 않으면 수정이 안되고 기존 데이터 그래도 사용 가능
+    author = wiseSayingRow.author,
+    goodLikeCount = wiseSayingRow.goodLikeCount,
+    badLikeCount = wiseSayingRow.badLikeCount,
+  } = req.body; // req.body에서 수정할 데이터를 받아 옴
+
+  // 수정한 데이터를 db에!
+  await pool.query(
+    `
+    UPDATE wise_saying
+    SET content = ?,
+    author = ?,
+    goodLikeCount = ?,
+    badLikeCount = ?
+    WHERE id = ?
+    `,
+    [content, author, goodLikeCount, badLikeCount, id]
+  );
+
+  // 수정 된 db 데이터를 다시 불러옴
+  const [[justModifiedWiseSayingRow]] = await pool.query(
+    `
+    SELECT *
+    FROM wise_saying
+    WHERE id = ?
+    `,
+    [id]
+  );
+
+  res.json({
+    resultCode: "S-1",
+    msg: "성공",
+    data: justModifiedWiseSayingRow,
   });
 });
 
